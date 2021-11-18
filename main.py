1039342ba60a83a807bcade3662be580ffa79c3d
kimnanhee <kimnanhee0225@gmail.com>
2021-11-18 11:38:46 +0900

    [Modify] main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import os, random, time
-questions = ["사과", "아보카도", "자동차", "비행기", "장미", "노트북", "휴대폰"]
+
+words = [] # 단어 리스트
+
+try:
+    with open("word.txt", "r", encoding="utf-8") as f: # 텍스트 파일 열기
+        while True:
+            line = f.readline() # 한 줄씩 읽어오기
+            if not line:
+                break
+            words.append(line.split()[0]) # 공백 기준으로 단어 자르기
+except Exception:
+    print("단어 파일을 여는데 문제가 발생했습니다.")
+    print("파일이 올바른 위치에 있는지 확인해주세요.")
+    exit(-1)
 
 while True:
     os.system("cls") # 화면 지우기
@@ -8,21 +21,21 @@ while True:
     gambler = int(input()) # 겜블러의 수
 
     liar = random.randint(1, gambler) # 라이버의 번호
-    question = random.randint(0, len(questions)-1) # 문제의 번호
+    word = random.randint(0, len(words)-1) # 문제의 번호
 
     for i in range(1, gambler+1):
         os.system("cls") # 화면 지우기
         print("{}번 겜블러의 제시어입니다.".format(i))
         input()
 
-        print(question, len(questions))
-        print("당신은 라이어입니다." if i==liar else questions[question])
+        print(word, len(words))
+        print("당신은 라이어입니다." if i==liar else words[word])
         time.sleep(2) # 2초
 
     os.system("cls") # 화면 지우기
     print("제시어를 확인하신 겜블러들은 순서대로 제시어에 대해서 설명해주세요.")
     print("순서대로 설명을 다 하셨을 경우, 엔터를 입력해주세요.")
-    input()
+    input() # 입력 기다리기
 
     arr = [0 for _ in range(gambler)] # 겜블러의 라이어 투표 결과
     for i in range(1, gambler+1):
@@ -34,16 +47,16 @@ while True:
     os.system("cls") # 화면 지우기
     print(arr)
 
-    if arr.index(max(arr))+1 == liar:
+    if arr.index(max(arr))+1 == liar: # 라이어 찾기 성공
         print("라이어를 찾아내는데에 성공했습니다.")
         print("라이어는 제시어라고 생각되는 단어를 입력해주세요.")
         print("만약 라이어가 제시어를 맞출 경우, 라이어의 승리입니다.")
         answer = input() # 제시어 입력
 
-        if answer==questions[question]:
+        if answer==words[word]:
             print("라이어가 제시어를 맞추었으므로, 라이어의 승리입니다.")
 
-    else:
+    else: #라이어 찾기 실패
         print("라이어를 찾아내는데에 실패했습니다.")
         print("라이어인 겜블러는 {}번 입니다.".format(liar))
 
